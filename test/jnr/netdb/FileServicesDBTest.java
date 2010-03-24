050f6f8a592df7ab69e766c2221d70c8b317686f
Wayne Meissner <wmeissner@gmail.com>
2010-03-24 22:04:53 +1000

    Remove autogen comments

--- test/jnr/netdb/FileServicesDBTest.java
+++ test/jnr/netdb/FileServicesDBTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package jnr.netdb;
 
@@ -14,7 +10,6 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author wayne
  */
 public class FileServicesDBTest {
 
