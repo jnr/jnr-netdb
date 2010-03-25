9b2a337299c7a083ffec9f053b4166beea6808db
Wayne Meissner <wmeissner@gmail.com>
2010-03-25 20:36:29 +1000

    Re-work NetDBIterator

--- src/jnr/netdb/NetDBIterator.java
+++ src/jnr/netdb/NetDBIterator.java
@@ -17,54 +17,63 @@ final class NetDBIterator implements java.util.Iterator<NetDBEntry> {
         this.reader = r instanceof BufferedReader ? (BufferedReader) r : new BufferedReader(r);
     }
 
-    NetDBEntry readNextEntry() {
-        try {
-            while (true) {
-                String s = reader.readLine();
-                if (s == null) {
-                    throw new NoSuchElementException("No more elements");
-                }
-                String[] line = s.split("#", 2);
-                // Skip empty lines, or lines that are all comment
-                if (line.length < 0 || line[0].isEmpty()) {
-                    continue;
-                }
-                String[] fields = line[0].trim().split("\\s+");
-                if (fields.length < 2 || fields[0] == null || fields[1] == null) {
-                    continue;
-                }
-                String serviceName = fields[0];
-                String data = fields[1];
-                List<String> aliases;
-                if (fields.length > 2) {
-                    aliases = new ArrayList<String>(fields.length - 2);
-                    for (int i = 2; i < fields.length; ++i) {
-                        if (fields[i] != null) {
-                            aliases.add(fields[i]);
-                        }
-                    }
-                } else {
-                    aliases = Collections.emptyList();
-                }
-                return new NetDBEntry(serviceName, data, aliases);
+    NetDBEntry readNextEntry() throws IOException {
+        String s = null;
+
+        while ((s = reader.readLine()) != null) {
+
+            String[] line = s.split("#", 2);
+            // Skip empty lines, or lines that are all comment
+            if (line.length < 0 || line[0].isEmpty()) {
+                continue;
             }
-        } catch (IOException ex) {
-            throw new NoSuchElementException(ex.getMessage());
+
+            String[] fields = line[0].trim().split("\\s+");
+            if (fields.length < 2 || fields[0] == null || fields[1] == null) {
+                continue;
+            }
+
+            String serviceName = fields[0];
+            String data = fields[1];
+            List<String> aliases;
+            if (fields.length > 2) {
+                aliases = new ArrayList<String>(fields.length - 2);
+                for (int i = 2; i < fields.length; ++i) {
+                    if (fields[i] != null) {
+                        aliases.add(fields[i]);
+                    }
+                }
+            } else {
+                aliases = Collections.emptyList();
+            }
+
+            return new NetDBEntry(serviceName, data, aliases);
         }
+        
+        return null;
     }
 
     public boolean hasNext() {
         try {
             return next != null || (next = readNextEntry()) != null;
-        } catch (NoSuchElementException ex) {
+        } catch (IOException ex) {
             return false;
         }
     }
 
     public NetDBEntry next() {
-        NetDBEntry s = next != null ? next : readNextEntry();
-        next = null;
-        return s;
+        try {
+            NetDBEntry s = next != null ? next : readNextEntry();
+            if (s == null) {
+                throw new NoSuchElementException("not found");
+            }
+            next = null;
+
+            return s;
+
+        } catch (IOException ex) {
+            throw new NoSuchElementException(ex.getMessage());
+        }
     }
 
     public void remove() {
