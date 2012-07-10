0b0c5af3ebd23454d39ba6bf5f198736bbc46644
Wayne Meissner <wmeissner@gmail.com>
2012-07-10 23:21:49 +1000

    Merge fix from master for 'Use _r() versions of getservbyname, getservbyport, etc for linux'

--- src/jnr/netdb/NativeServicesDB.java
+++ src/jnr/netdb/NativeServicesDB.java
@@ -18,11 +18,10 @@
 
 package jnr.netdb;
 
-import com.kenai.jaffl.CallingConvention;
-import com.kenai.jaffl.Library;
-import com.kenai.jaffl.LibraryOption;
-import com.kenai.jaffl.Platform;
-import com.kenai.jaffl.Pointer;
+import com.kenai.jaffl.*;
+import com.kenai.jaffl.annotations.Direct;
+import com.kenai.jaffl.annotations.Out;
+
 import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -38,9 +37,9 @@ import static com.kenai.jaffl.Platform.OS.*;
 /**
  *
  */
-final class NativeServicesDB implements ServicesDB {
+abstract class NativeServicesDB implements ServicesDB {
 
-    private final LibServices lib;
+    protected final LibServices lib;
 
     public NativeServicesDB(LibServices lib) {
         this.lib = lib;
@@ -71,18 +70,31 @@ final class NativeServicesDB implements ServicesDB {
                 Map<LibraryOption, Object> options = new HashMap<LibraryOption, Object>();
                 options.put(LibraryOption.CallingConvention, CallingConvention.STDCALL);
                 lib = Library.loadLibrary(LibServices.class, options, "Ws2_32");
+
             } else {
                 String[] libnames = os.equals(SOLARIS)
-                ? new String[] { "socket", "nsl", "c" }
-                : new String[] { "c" };
-                lib = Library.loadLibrary(LibServices.class, libnames);
+                    ? new String[] { "socket", "nsl", "c" }
+                    : new String[] { "c" };
+
+                if (os.equals(LINUX)) {
+                    lib = Library.loadLibrary(LinuxLibServices.class, libnames);
+                } else {
+                    lib = Library.loadLibrary(LibServices.class, libnames);
+                }
             }
 
+            NativeServicesDB services = os.equals(LINUX)
+                    ? new LinuxServicesDB(lib)
+                    : new DefaultNativeServicesDB(lib);
             // Try to lookup a service to make sure the library loaded and found the functions
-            lib.getservbyname("bootps", "udp");
-            lib.getservbyport(67, "udp");
+            if (services.getServiceByName("comsat", "udp") == null) {
+                return null;
+            }
+            services.getServiceByName("bootps", "udp");
+            services.getServiceByPort(67, "udp");
 
-            return new NativeServicesDB(lib);
+            return services;
+
         } catch (Throwable t) {
             Logger.getLogger(NativeServicesDB.class.getName()).log(Level.WARNING, "Failed to load native services db", t);
             return null;
@@ -96,41 +108,49 @@ final class NativeServicesDB implements ServicesDB {
         public final String proto = new UTF8StringRef();
     }
 
+    public static class LinuxServent extends UnixServent {
+        public static final int BUFLEN = 4096;
+        public final MemoryIO buf;
+
+        public LinuxServent() {
+            this.buf = MemoryIO.allocateDirect(BUFLEN, true);
+        }
+    }
+
     public static interface LibServices {
         UnixServent getservbyname(String name, String proto);
         UnixServent getservbyport(Integer port, String proto);
         UnixServent getservent();
         void endservent();
     }
-    
-    public Collection<Service> getAllServices() {
-        UnixServent s;
-        List<Service> allServices = new ArrayList<Service>();
 
-        try {
-            while ((s = lib.getservent()) != null) {
-                allServices.add(serviceFromNative(s));
-            }
-        } finally {
-            lib.endservent();
-        }
-
-        return allServices;
+    public static interface LinuxLibServices extends LibServices {
+        int getservbyname_r(String name, String proto, @Direct UnixServent servent,
+                                    Pointer buf, NativeLong buflen, @Out Pointer result);
+        int getservbyport_r(Integer port, String proto, @Direct UnixServent servent,
+                                    Pointer buf, NativeLong buflen, @Out Pointer result);
+        int getservent_r(@Direct UnixServent servent,
+                         Pointer buf, NativeLong buflen, Pointer result);
     }
 
-    private final Service serviceFromNative(UnixServent s) {
-        if (s == null) {
-            return null;
+    static int ntohs(int value) {
+        int hostValue =  ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN) ? Short.reverseBytes((short) value) : value;
+        if (hostValue < 0) {
+            // The byte flipping above will return numbers >= 32768 as a negative value,
+            // so they need to be converted back to a unsigned 16 bit value.
+            hostValue = ((hostValue & 0x7FFF) + 0x8000);
         }
 
-        // servent#port is in network byte order - but jaffl will assume it is in host byte order
-        // so it needs to be reversed again to be correct.
-        int port = ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN) ? Short.reverseBytes((short) s.port.get()) : s.port.get();
-        if (port < 0) {
-            // The s_port field is really an unsigned 16 bit quantity, but the
-            // byte flipping above will return numbers >= 32768 as a negative value,
-            // so they need to be converted back to a unsigned 16 bit value.
-            port = (int) ((port & 0x7FFF) + 0x8000);
+        return hostValue;
+    }
+
+    static int htons(int value) {
+        return ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN) ? Short.reverseBytes((short) value) : value;
+    }
+
+    static Service serviceFromNative(UnixServent s) {
+        if (s == null) {
+            return null;
         }
 
         List<String> emptyAliases = Collections.emptyList();
@@ -138,18 +158,86 @@ final class NativeServicesDB implements ServicesDB {
         final Collection<String> aliases = ((ptr = s.aliases.get()) != null)
                 ? StringUtil.getNullTerminatedStringArray(ptr) : emptyAliases;
 
-        return s != null ? new Service(s.name.get(), port, s.proto.get(), aliases) : null;
+        return new Service(s.name.get(), ntohs(s.port.get()), s.proto.get(), aliases);
     }
 
-    public Service getServiceByName(String name, String proto) {
-        return serviceFromNative(lib.getservbyname(name, proto));
+    static final class DefaultNativeServicesDB extends NativeServicesDB {
+        DefaultNativeServicesDB(LibServices lib) {
+            super(lib);
+        }
+
+        public Collection<Service> getAllServices() {
+            UnixServent s;
+            List<Service> allServices = new ArrayList<Service>();
+
+            try {
+                while ((s = lib.getservent()) != null) {
+                    allServices.add(serviceFromNative(s));
+                }
+            } finally {
+                lib.endservent();
+            }
+
+            return allServices;
+        }
+
+
+        public Service getServiceByName(String name, String proto) {
+            return serviceFromNative(lib.getservbyname(name, proto));
+        }
+
+        public Service getServiceByPort(Integer port, String proto) {
+            return serviceFromNative(lib.getservbyport(htons(port), proto));
+        }
     }
 
-    public Service getServiceByPort(Integer port, String proto) {
-        int nport = ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN) ? Short.reverseBytes(port.shortValue()) : port.shortValue();
-        
-        return serviceFromNative(lib.getservbyport(nport, proto));
-    }
+    static final class LinuxServicesDB extends NativeServicesDB {
+        private static final int BUFLEN = 4096;
+        private final LinuxLibServices lib;
+        private final Pointer buf;
 
+        LinuxServicesDB(LibServices lib) {
+            super(lib);
+            this.lib = (LinuxLibServices) lib;
+            this.buf = MemoryIO.allocateDirect(BUFLEN);
+        }
+
+        public synchronized Service getServiceByName(String name, String proto) {
+            UnixServent servent = new UnixServent();
+            Pointer result = MemoryIO.allocateDirect(8);
+            if (lib.getservbyname_r(name, proto, servent, buf, new NativeLong(BUFLEN), result) == 0) {
+                return result.getPointer(0) != null ? serviceFromNative(servent) : null;
+            }
+
+            throw new RuntimeException("getservbyname_r failed");
+        }
+
+        public synchronized Service getServiceByPort(Integer port, String proto) {
+            UnixServent servent = new UnixServent();
+            Pointer result = MemoryIO.allocateDirect(8);
+            if (lib.getservbyport_r(htons(port), proto, servent, buf, new NativeLong(BUFLEN), result) == 0) {
+                return result.getPointer(0) != null ? serviceFromNative(servent) : null;
+            }
+
+            throw new RuntimeException("getservbyport_r failed");
+        }
+
+        public synchronized Collection<Service> getAllServices() {
+            UnixServent s = new UnixServent();
+            List<Service> allServices = new ArrayList<Service>();
+            Pointer result = MemoryIO.allocateDirect(8);
+            NativeLong buflen = new NativeLong(BUFLEN);
+
+            try {
+                while (lib.getservent_r(s, buf, buflen, result) == 0 && result.getPointer(0) != null) {
+                    allServices.add(serviceFromNative(s));
+                }
+            } finally {
+                lib.endservent();
+            }
+
+            return allServices;
+        }
+    }
 
 }
